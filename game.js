dc45553305a1e5da329bdea194867dcb244893cf
Al <NA>
2019-02-20 19:29:42 -0500

    Limited board size, added createMatrix
    Made a fixed canvas board

--- game.js
+++ game.js
@@ -6,6 +6,12 @@ let dropCounter = 0;
 let dropInterval = 1000; //This is in MS, 1000 = 1 second
 let lastTime = 0;
 
+
+let scaleFactor = 20
+
+
+const board = createMatrix(canvas.width/scaleFactor,canvas.height/scaleFactor)
+
 // Tetrominos
 const tetrominos = [
     { //I Shape
@@ -71,19 +77,37 @@ const tetrominos = [
 // Player's Object
 const player ={
     pos: { x: 5, y: 0},
-    tetromino: tetrominos[Math.floor(Math.random() * 7)]
+    tetromino: getTetromino()
 }
 
-//Handle window resize via event listener
-window.addEventListener('resize',resizeCanvas,false) 
+function getTetromino(){
+        return tetrominos[Math.floor(Math.random() * 7)]
+}
+
+function createMatrix(w,h){
+    const matrix =[];
+    while(h--){
+        matrix.push(new Array(w).fill(0))
+    }
+    return matrix;
+}
 
 function resizeCanvas(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.scale(50,50) //Scale each pixel by 50 w/h
+    //  canvas.width = window.innerWidth;
+    //  canvas.height = window.innerHeight;
+    canvas.width = 240;
+    canvas.height = 400;
+    ctx.scale(scaleFactor,scaleFactor) //Scale each pixel 
     update()
 }
 
+function draw(){
+    //Draw a black canvas
+    ctx.fillStyle = '#000'; 
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    //Draw the current Tetromino of the player
+    drawTetromino(player.tetromino,player.pos)
+}
 
 function drawTetromino(tetromino,offset){
     tetromino.shape.forEach((row,y)=>{
@@ -97,19 +121,16 @@ function drawTetromino(tetromino,offset){
 }
 
 
+function moveTetromino(offset){
+    //This should only affect the x cor
+    let newX = player.pos.x + offset
+    //TODO check for collisions
+}
+
 function tick(){
     player.pos.y++; //Increase the Y value
     //Check the bounds
-
     dropCounter = 0; //Reset the counter otherwise the piece will free fall
-}
-
-function draw(){
-    //Draw a black canvas
-    ctx.fillStyle = '#000'; 
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-    //Draw the current Tetromino of the player
-    drawTetromino(player.tetromino,player.pos)
 }
 
 
@@ -121,8 +142,22 @@ function update(time = 0){
     }
     lastTime = time;
     draw()
-    requestAnimationFrame(update); //Call run before next frame
+    requestAnimationFrame(update); //Call update before next frame
 }
 
 
+
+//Event Handlers
+window.addEventListener('resize',resizeCanvas,false)  //Handles resizes
+window.addEventListener('keydown',event=>{
+    switch(event.keyCode){
+        case 37: //Left arrow
+            moveTetromino(-1)
+            break;
+        case 39: //Right arrow
+            moveTetromino(1)
+            break;
+       
+    }
+})
 resizeCanvas();
